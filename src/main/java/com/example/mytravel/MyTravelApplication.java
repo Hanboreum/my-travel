48e6e507c4b5c09a462041f1f9c2dee4c9cd40ea
fullm <matoma1_@naver.com>
2024-04-28 15:27:52 +0900

    Initial commit

---
+++ src/main/java/com/example/mytravel/MyTravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTravelApplication.class, args);
+	}
+
+}
